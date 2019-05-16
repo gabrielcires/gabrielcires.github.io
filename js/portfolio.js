0f1a7ed9ca0295d69cd4df0263756718f2fbdc74
Gabriel Cires <gabrielcireslopez@gmail.com>
2019-05-16 20:18:45 +0200

    Animaciones en proceso

--- js/portfolio.js
+++ js/portfolio.js
@@ -14,13 +14,12 @@ window.onload = function () {
         }, 500);
     });
 
-
     $(window).scroll(function () {
         var scrollDistance = $(window).scrollTop();
 
         // Assign active class to nav links while scrolling
         $('.section-display').each(function (i) {
-            if ($(this).position().top <= scrollDistance) {
+            if ($(this).position().top <= scrollDistance + 150) {
                 $('.menu-list li a.active').removeClass('active');
                 $('.menu-list li a').eq(i).addClass('active');
             }
@@ -28,3 +27,26 @@ window.onload = function () {
     }).scroll();
 
 }
+
+function showFooter(x) {
+    let footer = document.getElementById("footer");
+
+    x.classList.toggle("minus");
+    footer.classList.toggle("show");
+};
+
+//Appear js
+let animElement =  document.getElementsByClassName('pre-anim');
+
+appear({
+    init: function init(){
+      console.log('dom is ready');
+    },
+    elements: function elements(){
+      return animElement;
+    },
+    appear: function appear(el){
+        el.classList.add("do-anim");
+      },
+    bounds: -200,
+  });
