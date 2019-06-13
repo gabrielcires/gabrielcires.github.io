bacc5591d2246191d98fd080b18d2e0805105625
Gabriel Cires <gabrielcireslopez@gmail.com>
2019-06-13 18:35:57 +0200

    Retoques finales, cambio de informacion y pulimiento

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,23 @@
 window.onload = function () {
     // prueba de animaciones
-    // setTimeout(function(){
-    //     var a = document.getElementById("iniAnim");
-    //     a.className += " anim";       
-    // }, 3500);
+    setTimeout(function () {
+        
+        //Appear js
+        let animElement = document.getElementsByClassName('pre-anim');
+
+        appear({
+            init: function init() {
+                console.log('dom is ready');
+            },
+            elements: function elements() {
+                return animElement;
+            },
+            appear: function appear(el) {
+                el.classList.add("do-anim");
+            },
+            bounds: -180,
+        });
+    }, 1500);
 
     // smothscroll
     $(document).on('click', 'a[href^="#"]', function (event) {
@@ -14,10 +28,10 @@ window.onload = function () {
         }, 500);
     });
 
+    // Agregar clases al menu al entrar en diferentes secciones
     $(window).scroll(function () {
         var scrollDistance = $(window).scrollTop();
 
-        // Assign active class to nav links while scrolling
         $('.section-display').each(function (i) {
             if ($(this).position().top <= scrollDistance + 150) {
                 $('.menu-list li a.active').removeClass('active');
@@ -28,25 +42,12 @@ window.onload = function () {
 
 }
 
+
+
+
 function showFooter(x) {
     let footer = document.getElementById("footer");
 
     x.classList.toggle("minus");
     footer.classList.toggle("show");
 };
-
-//Appear js
-let animElement =  document.getElementsByClassName('pre-anim');
-
-appear({
-    init: function init(){
-      console.log('dom is ready');
-    },
-    elements: function elements(){
-      return animElement;
-    },
-    appear: function appear(el){
-        el.classList.add("do-anim");
-      },
-    bounds: -180,
-  });
